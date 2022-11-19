9e5ee30795b0fd545660fa6f4ba2d09b63a8824c
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-11-19 20:08:25 +0100

    Fix button text being blurry

--- src/TitleScene.cpp
+++ src/TitleScene.cpp
@@ -5,6 +5,7 @@
 #include "ExitScene.h"
 #include "Label.h"
 #include "ResponsiveSprite.h"
+#include "InputField.h"
 
 namespace yuzu
 {
@@ -13,15 +14,15 @@ namespace yuzu
         bool success = true;
         std::cout << "TitleScene::enter()" << std::endl;
 
-        fruitwork::Button *startButton = fruitwork::Button::getInstance(50, 400, 390, 100, "Start");
+        fruitwork::Button *startButton = fruitwork::Button::getInstance(50, 400, 240, 48, "Start");
         startButton->registerCallback([](fruitwork::Button *src)
                                       {
                                           std::cout << "Start button clicked" << std::endl;
                                           fruitwork::sys.setNextScene(fruitwork::ExitScene::get_instance());
                                       });
 
-        startButton->setTextColor({255, 0, 0, 255});
-        startButton->setColor({0, 255, 0, 255});
+        startButton->setTextColor({134, 140, 158, 255});
+        startButton->setColor({226, 255, 191, 255});
 
         fruitwork::Label *label = fruitwork::Label::getInstance(0, 50, 1200, 900, "yuzu!catch");
         label->setColor({255, 204, 242, 255});
@@ -29,11 +30,14 @@ namespace yuzu
         label->setFontSize(100);
         label->setFont("Kepler 296.ttf");
 
+        fruitwork::InputField *inputField = fruitwork::InputField::getInstance(50, 300, 390, 48, "Enter your name...");
+
         fruitwork::Sprite *background = fruitwork::Sprite::getInstance(0, 0, 1200, 900, "background.png");
 
         add_component(background);
         add_component(label);
         add_component(startButton);
+        add_component(inputField);
 
         return success;
     }
