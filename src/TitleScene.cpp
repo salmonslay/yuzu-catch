b2a5b1724cd6821d18c2d873783385ee693006d5
Fabian Lindgren <lindgrenfabian@gmail.com>
2023-01-02 23:17:05 +0100

    Update main menu

--- src/TitleScene.cpp
+++ src/TitleScene.cpp
@@ -18,24 +18,34 @@ namespace yuzu
         bool success = true;
         SDL_Log("Entering TitleScene...");
 
-        fruitwork::Button *startButton = fruitwork::Button::getInstance(50, 400, 240, 48, "Start");
-        startButton->registerCallback([](fruitwork::Button *src)
-                                      {
-                                          std::cout << "Start button clicked" << std::endl;
-                                          fruitwork::sys.setNextScene(GalleryScene::getInstance());
-                                      });
-
-        startButton->setTextColor({134, 140, 158, 255});
-        startButton->setColor({226, 255, 191, 255});
 
         fruitwork::Label *label = fruitwork::Label::getInstance(0, 50, constants::gScreenWidth, constants::gScreenHeight, "yuzu!catch");
         label->setColor({255, 204, 242, 255});
         label->setAlignment(fruitwork::Label::Alignment::CENTER);
         label->setFontSize(100);
+        addComponent(label, 0);
 
-        fruitwork::InputField *inputField = fruitwork::InputField::getInstance(50, 300, 390, 48, "Enter your name...");
+        fruitwork::Button *startButton = fruitwork::Button::getInstance(400, 400, 400, 48, "Start Game");
+        startButton->registerCallback([](fruitwork::Button *src)
+                                      {
+                                          fruitwork::sys.setNextScene(GalleryScene::getInstance());
+                                      });
+        startButton->setColor({255, 204, 232, 255});
+        addComponent(startButton, 1);
 
-        fruitwork::Sprite *background = fruitwork::Sprite::getInstance(0, 0, constants::gScreenWidth, constants::gScreenHeight, fruitwork::ResourceManager::getTexturePath("background.png"));
+        fruitwork::Button *exitButton = fruitwork::Button::getInstance(400, 500, 400, 48, "Exit Game");
+        exitButton->registerCallback([](fruitwork::Button *src)
+                                     {
+                                         SDL_Event quitEvent;
+                                         quitEvent.type = SDL_QUIT;
+                                         SDL_PushEvent(&quitEvent);
+                                     });
+        exitButton->setColor({255, 204, 232, 255});
+        addComponent(exitButton, 1);
+
+        fruitwork::Sprite *background = fruitwork::Sprite::getInstance(0, 0, constants::gScreenWidth, constants::gScreenHeight,
+                                                                       fruitwork::ResourceManager::getTexturePath("background.png"));
+        addComponent(background, -10);
 
         // test scene button
         fruitwork::Button *testButton = fruitwork::Button::getInstance(10, 842, 240, 48, "Visual tests...");
@@ -45,12 +55,7 @@ namespace yuzu
                                      });
         testButton->setColor({255, 255, 255, 128});
         testButton->setTextColor({0, 0, 0, 128});
-
-        addComponent(background);
-        addComponent(label);
-        addComponent(startButton);
-        addComponent(inputField);
-        addComponent(testButton);
+        addComponent(testButton, 1);
 
         return success;
     }
